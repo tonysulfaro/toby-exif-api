a0c30cdbfc5d71871c822aaec0c0a67feb9b0eee
Tony Sulfaro <tony116523@gmail.com>
2020-02-03 14:11:16 -0500

    add cors headers

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ express()
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();}
+    next()})
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
