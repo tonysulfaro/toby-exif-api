ed0618f40cc0f73519db2c32eb235caa2d09dbe5
Tony Sulfaro <tony116523@gmail.com>
2020-02-03 13:56:45 -0500

    add Procfile

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const cors = require('cors')
+var ExifImage = require('exif').ExifImage
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
