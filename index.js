0fece2538d47125f5d2bee996b3c0a1e3ec553f2
Tony Sulfaro <tony116523@gmail.com>
2020-02-03 14:14:04 -0500

    add cors headers

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ var ExifImage = require('exif').ExifImage
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*')
-    res.header("Access-Control-Allow-Credentials", true)
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
-    next()
-  })
+  .use(cors())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
