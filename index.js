0f92cf519cf242614d21925148e645adc8830a41
Tony Sulfaro <tony116523@gmail.com>
2020-02-03 14:01:06 -0500

    add Procfile

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .post('/cords', cors(), function (req, res) {
+    // res.send(console.dir(req.files))
+    console.log(req)
+    console.log('now time for walleye image')
+
+    try {
+      ExifImage({ image: 'walleye.jpg' }, function (error, exifData) {
+        if (error) {
+          console.log('Error: ' + error.message)
+        }
+        else {
+          console.log(exifData) // Do something with your data!
+          res.send(exifData)
+        }
+      })
+    } catch (error) {
+      console.log('Error: ' + error.message)
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT}`))
